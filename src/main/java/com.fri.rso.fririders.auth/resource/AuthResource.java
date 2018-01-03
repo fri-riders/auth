ef353b211303f8a0c4b24a759631fdbf7141eb88
Tomi Sebjanic <ts4293@student.uni-lj.si>
2018-01-03 21:09:13 +0100

    Implement JWT

--- src/main/java/com.fri.rso.fririders.auth/resource/AuthResource.java
+++ src/main/java/com.fri.rso.fririders.auth/resource/AuthResource.java
@@ -1,14 +1,21 @@
 package com.fri.rso.fririders.auth.resource;
 
 import com.kumuluz.ee.logs.cdi.Log;
+import com.nimbusds.jose.*;
+import com.nimbusds.jose.crypto.MACSigner;
+import com.nimbusds.jose.crypto.MACVerifier;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
 
 import javax.enterprise.context.RequestScoped;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.text.ParseException;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.UUID;
 
 @RequestScoped
 @Consumes(MediaType.APPLICATION_JSON)
@@ -17,9 +24,63 @@ import javax.ws.rs.core.Response;
 @Log
 public class AuthResource {
 
+    private String issuer;
+    private byte[] sharedSecret;
+
+    public AuthResource() {
+        this.sharedSecret = "t6i8wziwxjAhAtqCp4QLbeb2T9sz9VyY".getBytes();
+        this.issuer = "http://auth.fririders.com";
+    }
+
     @GET
     public Response test() {
         return Response.ok().build();
     }
 
+    @POST
+    @Path("issue")
+    public Response issueToken(String jsonPayload) throws JOSEException {
+        HashMap<String, String> payload = Helpers.jsonToMap(jsonPayload);
+        Date now = new Date();
+
+        assert payload != null && payload.get("email") != null;
+
+        JWTClaimsSet claimsSet = new JWTClaimsSet.Builder()
+                .subject(payload.get("email"))
+                .issuer(this.issuer)
+                .audience(Arrays.asList("http://app.fririders.com", "http://users.fririders.com"))
+                .expirationTime(new Date(now.getTime() + 60 * 1000))
+                .issueTime(now)
+                .notBeforeTime(now)
+                .jwtID(UUID.randomUUID().toString())
+                .claim("ROLE", "USER")
+                .claim("EMAIL", payload.get("email"))
+                .build();
+
+        JWSSigner signer = new MACSigner(this.sharedSecret);
+        SignedJWT signedJWT = new SignedJWT(new JWSHeader(JWSAlgorithm.HS256), claimsSet);
+        signedJWT.sign(signer);
+
+        return Response.ok(Helpers.buildTokenMessage(signedJWT.serialize(), payload.get("email"))).build();
+    }
+
+    @POST
+    @Path("verify")
+    public Response verifyToken(String jsonPayload) throws ParseException, JOSEException {
+        HashMap<String, String> payload = Helpers.jsonToMap(jsonPayload);
+
+        assert payload != null && payload.get("token") != null && payload.get("email") != null;
+
+        SignedJWT signedJWT = SignedJWT.parse(payload.get("token"));
+        JWSVerifier verifier = new MACVerifier(this.sharedSecret);
+
+        boolean valid = signedJWT.verify(verifier) &&
+                signedJWT.getJWTClaimsSet().getSubject().equals(payload.get("email")) &&
+                signedJWT.getJWTClaimsSet().getClaim("EMAIL").equals(payload.get("email")) &&
+                signedJWT.getJWTClaimsSet().getIssuer().equals(this.issuer) &&
+                new Date().before(signedJWT.getJWTClaimsSet().getExpirationTime()) &&
+                new Date().after(signedJWT.getJWTClaimsSet().getNotBeforeTime());
+
+        return (valid ? Response.ok() : Response.status(Response.Status.UNAUTHORIZED)).build();
+    }
 }
---
+++ src/main/java/com.fri.rso.fririders.auth/resource/Helpers.java
@@ -0,0 +1,54 @@
+package com.fri.rso.fririders.auth.resource;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+public class Helpers {
+
+    /**
+     * @param message Error message
+     * @return Json formatted string
+     */
+    public static String buildErrorJson(String message) {
+        return "{ " +
+                "\"error\": true," +
+                "\"message\": \"" + message + "\"" +
+                " }";
+    }
+
+    /**
+     * @param jsonString String to parse
+     * @return HashMap in JSON form
+     */
+    public static HashMap<String, String> jsonToMap(String jsonString) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+
+            return objectMapper.readValue(jsonString, new TypeReference<HashMap<String, String>>() {});
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * @param message Message to send
+     * @return JSON object with message
+     */
+    public static String buildMessageJson(String message) {
+        return "{ " +
+                "\"message\": \"" + message + "\"" +
+                " }";
+    }
+
+    public static String buildTokenMessage(String token, String email) {
+        return "{ " +
+                "\"token\": \"" + token + "\"," +
+                "\"email\": \"" + email + "\"" +
+                " }";
+    }
+
+}
